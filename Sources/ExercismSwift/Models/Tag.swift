99dec42472e5fdecda83533c5d41f237649e0b03
angiemugo <angela@basecamp.com>
2022-10-12 15:29:29 +0300

    add tags resources

--- Sources/ExercismSwift/Models/Tag.swift
+++ Sources/ExercismSwift/Models/Tag.swift
@@ -4,6 +4,26 @@
 
 import Foundation
 
-public struct Tag {
+public struct Tag: Decodable {
+    public var type: String
+    public var tags: [String]
+}
 
+extension Tag {
+    static public func loadTags() -> [Tag] {
+        if let bundledUrl = Bundle.main.url(forResource: "Tags", withExtension: "json") {
+            if let data = FileManager.default.contents(atPath: bundledUrl.path) {
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    return try decoder.decode([Tag].self, from: data)
+                } catch {
+                    print(error)
+                    return []
+                }
+            }
+            return []
+        }
+        return []
+    }
 }
