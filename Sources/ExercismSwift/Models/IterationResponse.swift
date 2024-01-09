9cae641f298e13bb42700cb7b0af0bea5cb0e8cc
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-01-09 10:04:50 +0200

    Remote file commentS

--- Sources/ExercismSwift/Models/IterationResponse.swift
+++ Sources/ExercismSwift/Models/IterationResponse.swift
@@ -1,12 +1,9 @@
 //
-//  File.swift
-//  
-//
 //  Created by Cédric Bahirwe on 08/01/2024.
 //
 
 import Foundation
 
 public struct IterationResponse: Decodable {
-    let iterations: [Iteration]
+    public let iterations: [Iteration]
 }
